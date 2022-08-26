1fec7cd9572aa35a649e4a914208f86c0923f797
e-a-w- <eawatkins.0@gmail.com>
2022-08-26 01:46:47 -0700

    2.2 add show page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,10 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def show
+    @product = Product.find_by(id: params["id"])
+
+    redirect_to products_path if @product.nil?
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,24 @@ class Product < ApplicationRecord
   validates :description, length: { maximum: 200 }
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
+  def display_attributes
+    attributes.tap do |attr|
+      attr['price'] = formatted_price
+      attr['name'] = attr['name'].upcase
+      attr['created_at'] = format_date(attr['created_at'])
+      attr['updated_at'] = format_date(attr['updated_at'])
+      attr['last_sold_at'] = format_date(attr['last_sold_at'])
+    end
+  end
+
+  def formatted_price
+    "$#{'%.2f' % (price.to_i / 100.0)}"
+  end
+
+  private
+
+  def format_date(date)
+    date.utc.strftime("%A %b %-d, %l:%M%P %z").squish
+  end
 end
