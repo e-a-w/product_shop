56b4b97c162d7f277c2ca01cb2e171766a331387
e-a-w- <eawatkins.0@gmail.com>
2022-08-26 00:48:56 -0700

    2.1 products index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 50.times do |i|
   Product.create(
     name: Faker::Commerce.unique.product_name,
-    description: (i % 3).zero? && Faker::Company.catch_phrase,
+    description: (i % 3).zero? ? Faker::Company.catch_phrase : nil,
     price: Faker::Number.within(range: 1..150),
     quantity: Faker::Number.within(range: 0..100),
     last_sold_at: Faker::Date.between(from: 1.year.ago, to: Date.yesterday)
